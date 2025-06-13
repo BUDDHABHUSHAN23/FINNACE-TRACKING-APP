d5fdfc63a373ccb256784f640bd7d7f1ed1c9852
BUDDHABHUSHAN23 <buddhabhsushan.waghmare.2322@gmail.com>
2025-06-13 10:58:24 +0530

    Starting to create the Frontend

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import {Navigate, Route, Routes} from 'react-router-dom'; // Import necessary components from react-router-dom
+import {Navigate, Route, Routes} from 'react'; // Import necessary components from react-router-dom
 
 function App() {
 
 
   return (
-    <main>   // Main application component
-      <div> // Main content area
-        <Routes> // Define routes for the application
+    <main>  
+      <div> 
+        {/* <Routes> // Define routes for the application
           <Route path="/" element={<Navigate to="/home" />} /> // Redirect root path to /home
           <Route path="/home" element={<h1>Home Page</h1>} /> // Home page route
           <Route path="/about" element={<h1>About Page</h1>} /> // About page route
           <Route path="/contact" element={<h1>Contact Page</h1>} /> // Contact page route
           <Route path="*" element={<h1>404 Not Found</h1>} /> // Catch-all route for 404 errors
-        </Routes>
+        </Routes> */}
 
       </div>
       
